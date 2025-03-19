f1f90bede7eeb8c4673566a4ec8fe7b6f3918fea
Edie Cahill <ediecahill@macbookpro.lan>
2025-03-19 12:29:03 -0700

    Edie Cahill -eac2287

--- static/scoreboard.js
+++ static/scoreboard.js
@@ -32,7 +32,8 @@ function increase_score(id){
     contentType: "application/json; charset=utf-8",
     data : JSON.stringify(team_id),
     success: function(result){
-        
+        let board = result["scoreboard"]
+        display_scoreboard(board);
     },
     error: function(request, status, error){
         console.log("Error");
